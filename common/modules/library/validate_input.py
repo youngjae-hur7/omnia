7840f79d406b0dbc4dadb5a6fce76407cf549192
aditya-deshpande <aditya.deshpande1@dell.com>
2025-04-14 10:00:10 +0530

    Input validator restructuring

--- common/modules/library/validate_input.py
+++ common/modules/library/validate_input.py
@@ -82,7 +82,9 @@ def main():
     project_name = module.params["project_name"]
     tag_names = eval(module.params["tag_names"])
     single_files = module.params["files"]
-    schema_base_file_path = os.path.join(os.getcwd(),'schema/')
+    # schema_base_file_path = os.path.join(os.getcwd(),'')
+    # schema_base_file_path = "./module_utils/input_validation/schema/"
+    schema_base_file_path = '../module_utils/input_validation/schema'
     directory_path = os.path.join(omnia_base_dir, project_name)
     input_file_inventory = config.input_file_inventory
     passwords_set = config.passwords_set
