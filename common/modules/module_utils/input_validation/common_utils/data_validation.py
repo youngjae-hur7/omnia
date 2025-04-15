3af059266afa3b6e2e4a47dc3cc764b95003f863
aditya-deshpande <aditya.deshpande1@dell.com>
2025-04-15 17:01:01 +0530

    Input validator restructuring

--- common/modules/module_utils/input_validation/common_utils/data_validation.py
+++ common/modules/module_utils/input_validation/common_utils/data_validation.py
@@ -93,12 +93,15 @@ def schema(input_file_path, schema_file_path, passwords_set, omnia_base_dir, pro
     except jsonschema.exceptions.SchemaError as se:
         message = f"Internal schema validation error: {se.message}"
         logger.error(message)
+        return False
     except ValueError as ve:
         message = f"Value error: {ve}"
         logger.error(message)
+        return False
     except Exception as e:
         message = f"An unexpected error occurred: {e}"
         logger.error(message)
+        return False
 
 # Code to run the L2 validation validate_input_logic function.
 def logic(input_file_path, logger, module, omnia_base_dir, module_utils_base, project_name):
